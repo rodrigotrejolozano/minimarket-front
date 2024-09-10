5cbae51cc8b9f8f1f4b4d55fad00debf7826edc4
RoroDev <rodrigotrejo098@gmail.com>
2024-09-10 13:13:00 -0500

    feat(api-product): added API routes for product (GET/POST)

--- src/pages/api/product/index.ts
+++ src/pages/api/product/index.ts
@@ -1,17 +1,24 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 import api, { ApiResponse } from '@/services/api'
 import { handleError } from '@/utils/handlerError'
-import { Category } from '@/types/category.types'
+import { Product } from '@/types/product.types'
 
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const baseUrl = process.env.API_URL + `/api/v1/categories`
+    const baseUrl = process.env.API_URL + `/api/v1/products`
     try {
         let response;
 
         switch (req.method) {
             case 'GET':
-                response = await api.get<ApiResponse<Category[]>>(baseUrl)
+                response = await api.get<ApiResponse<Product[]>>(baseUrl)
                 return res.status(200).json(response.data.data)
+            case 'POST':
+                const { name, categoryId } = req.body.data
+                response = await api.post<ApiResponse<Product>>(baseUrl,
+                    { name, categoryId }
+                )
+                return res.status(201).json(response.data.data)
+
             default:
                 return res.status(405).json({ code: '405', message: 'Method not allowed' })
         }
