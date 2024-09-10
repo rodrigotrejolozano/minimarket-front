a3f5f33a77588ac9f87abf8a793865b7574005f0
RoroDev <rodrigotrejo098@gmail.com>
2024-09-10 13:27:42 -0500

    feat(pages): added custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import PageLayout from '@/components/layout/PageLayout'
+import { NextPage } from 'next'
+import React from 'react'
+
+const ErrorNotFound:NextPage = () => {
+  return (
+    <PageLayout>
+        <h1>404 - Page Not Found</h1>
+         
+    </PageLayout>
+  )
+}
+
+export default ErrorNotFound
