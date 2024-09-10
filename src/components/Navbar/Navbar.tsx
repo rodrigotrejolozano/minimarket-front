d5b5fd1bcc5b29d92cac3abac2b3fdc5e904017a
RoroDev <rodrigotrejo098@gmail.com>
2024-09-10 13:25:34 -0500

    feat(navbar): added navbar with ModCreateProduct functionality

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import ModCreateProduct from '../Modals/ModCreateProduct'
+import { useQueryClient } from 'react-query'
 
-const Navbar:React.FC = () => {
+const Navbar: React.FC = () => {
+  const query = useQueryClient()
 
+  const handleRefresh = useCallback(() => {
+    query.invalidateQueries(["productsAll"])
+    query.invalidateQueries(["productsByCategory"])
+  }, [query])
   return (
     <>
       <nav className="fixed z-20 top-0 left-0 right-0 bg-white bg-opacity-80 backdrop-filter backdrop-blur-lg shadow-md ">
@@ -10,6 +17,9 @@ const Navbar:React.FC = () => {
             <span className="text-2xl font-bold text-blue-800">MiniMarket</span>
             <div className="flex space-x-4">
 
+              <ModCreateProduct
+                onRefresh={handleRefresh}
+              />
             </div>
           </div>
         </div>
